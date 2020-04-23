863b1f7f4ac1197cf17d517c17415f01968eaa53
Federico Ottena <federico.ottena@aalto.it>
2020-04-23 18:04:15 +0200

    first commit

---
+++ MvcMovie/Controllers/HelloWordController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 10)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
